4ec0d2d8bc51785350b1ae16f02895bc989176fc
Hongyuan Wang <710136012@qq.com>
2015-07-25 10:02:01 +0800

    delete "length==0"

--- Contains-Duplicate/Solution.java
+++ Contains-Duplicate/Solution.java
@@ -1,8 +1,5 @@
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums.length == 0) {
-            return false;
-        }
         HashSet<Integer> bucket = new HashSet();
         for(int num : nums) {
             if(bucket.contains(num)) {
